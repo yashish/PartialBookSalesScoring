de0231c9f47b451d9c9f90d9d291e1908f480078
Ashish Yengkhom <ashish.yengkhom@gmail.com>
2025-12-01 08:29:05 -0500

    Update ScoringService to use decimal for monetary calculations

--- obj/Debug/net9.0/PartialBookSalesScoring.AssemblyInfo.cs
+++ obj/Debug/net9.0/PartialBookSalesScoring.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PartialBookSalesScoring")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52d19812f47476e882ffd954a6629cc252f2c284")]
 [assembly: System.Reflection.AssemblyProductAttribute("PartialBookSalesScoring")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PartialBookSalesScoring")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
